91dd16499015710dce6a708a577757165853c25f
Valdas Vabolis <thisisvaldasvabolis@gmail.com>
2017-06-25 12:28:38 +0000

    Imported bootstrap gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # home page points to pages controller, home method
+  # home page points to pages controller, home action
   root to: 'pages#home'
+  # on get request, go to pages controller and run about action
   get 'about', to: 'pages#about'
 end
