634c24fba9445b40f52acc088ae4bc70b18743a6
Valdas Vabolis <thisisvaldasvabolis@gmail.com>
2017-06-26 14:38:45 +0000

    Updated about page and logo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
     def home
-        
+        puts "You're in the home page"
     end
     
     def about
-        
+        puts "You're in the about page"
     end
 end
