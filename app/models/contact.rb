528636cc8f0c3caf97bb02591048e4ef6edec5bd
Valdas Vabolis <thisisvaldasvabolis@gmail.com>
2017-06-27 13:29:25 +0000

    Added form validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ActiveRecord::Base
-  
+  # make sure contact form contains these fields
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :comments, presence: true
 end
