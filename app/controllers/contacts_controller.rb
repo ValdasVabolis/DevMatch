e91647f2de5efd9642ad6d48ead7124bd3379074
Valdas Vabolis <thisisvaldasvabolis@gmail.com>
2017-06-27 08:33:33 +0000

    Added create action to save to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,22 @@
 class ContactsController < ApplicationController
   def new
+    # create a blank contact object
+    @contact = Contact.new
+  end
+  # saves contact form to db
+  def create
+    @contact = Contact.new(contact_params)
+    # if save successful
+    if @contact.save
+      # redirect to blank form with success message
+      redirect_to new_contact_path, notice: "Message sent!"
+    else
+      # redirect to blank form with failure message
+      redirect_to new_contact_path, notice: "Something went wrong. Please try again!"
+    end
+  end
+  
+  def contact_params
+    params.require(:contact).permit(:name, :email, :comments)
   end
 end
