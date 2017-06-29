0826632e9028a5fc5105148747755b5dd2305f4a
Valdas Vabolis <thisisvaldasvabolis@gmail.com>
2017-06-29 18:03:46 +0000

    Added comments for elaboration

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,32 +1,42 @@
 class ContactsController < ApplicationController
   # Rails looks for corresponding view file (new.html.erb)
   # when routes.rb calls this action, that's how we display
-  # the webpage we want. 
+  # the webpage we want.
+  
+  # GET request to /contact-us
+  # Show new contact form
   def new
     # create a blank contact object
     @contact = Contact.new
   end
   # saves contact form to db
+  # POST request to /contacts
   def create
+    # mass assign form fields into Contact object
     @contact = Contact.new(contact_params)
-    # if save successful
+    # try to save Contact object to db
     if @contact.save
-      # send the email
+      # store form fields via parameters into variables
       name = params[:contact][:name]
       email = params[:contact][:email]
       body = params[:contact][:comments]
+      # plug variables into Contact Mailer email method and send email
       ContactMailer.contact_email(name, email, body).deliver
-      # redirect to blank form with success message
+      # store success message in flash hash
       flash[:success] = "Message sent!"
+      # redirect to new action
       redirect_to contact_us_path
     else
-      # redirect to blank form with failure message
+      # if Contact object doesn't save
+      # store errors in flash hash and redirect to the new action
       flash[:danger] = @contact.errors.full_messages.join(", ")
       redirect_to contact_us_path
     end
   end
   
   private
+    # to collect data from form, we need to use
+    # strong parameters and whitelist the form fields
     def contact_params
       params.require(:contact).permit(:name, :email, :comments)
     end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 class Contact < ActiveRecord::Base
   # make sure contact form contains these fields
+  # ( contact form validations )
   validates :name, presence: true
   validates :email, presence: true
   validates :comments, presence: true
