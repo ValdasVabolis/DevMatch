a102eb1eed9147c51c44be8e2605b16701b69967
Valdas Vabolis <thisisvaldasvabolis@gmail.com>
2017-07-04 19:17:03 +0000

    Added profiles create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,4 +5,26 @@ class ProfilesController < ApplicationController
     # Create a profile instance to be used in our profile form
     @profile = Profile.new
   end
+  # POST to /users/:user_id/profile
+  def create
+    # Ensure that we have the user who is filling out form
+    @user = User.find(params[:user_id])
+    # Create profile linked to this specific user
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
+      flash[:success] = "Profile updated successfully."
+      redirect_to root_path
+    else
+      flash[:danger] = "Something went wrong. Please try again!"
+      render action: :new
+    end
+  end
+  
+  private
+    def profile_params
+      # Whitelist forms for security (only accept these params in our db)
+      params.require(:profile).permit(:first_name, :last_name, :job_title,
+                                      :phone_number, :contact_email, :description)
+    end
+  
 end
