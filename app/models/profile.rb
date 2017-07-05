3e43b055514c6b079edb5c54324b7ff7a2268c46
Valdas Vabolis <thisisvaldasvabolis@gmail.com>
2017-07-05 14:41:37 +0000

    Added avatar column to Profiles table

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,8 @@
 class Profile < ActiveRecord::Base
   # each profile belongs to user (connects to user class)
   belongs_to :user
+  # create association for image (imagemagick avatar)
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" },
+                             default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
